806a6be68fcaad36c5635650d73521920e8bdb11
wowjdql98 <105402290+wowjdql98@users.noreply.github.com>
2022-06-22 16:39:20 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,41 +1,37 @@
-
-$(function () {
-    //시간재생 - 매 1초마다 재생
-    setInterval(function () {
-        let d = new Date();
-        let h = d.getHours(); //시간
-        let m = d.getMinutes(); //분
-        let s = d.getSeconds(); //초
-
-        //0 ~ 9 까지는 0을 앞에 표시하기
-        if (s < 10) {
-            s = '0' +  5;
-        }
-        
-        if (h < 10) {
-            h = '0' +  5;
-        }
-        if (m < 10) {
-            m = '0' +  5;
-        }
-        
-        //오전 오후 표시
-
+$(function(){
+    // 시간재생 - 매 1초마다 재생
+    setInterval(function(){
+      // app.js
+      let d = new Date();
+      let h = d.getHours(); // 시간
+      let m = d.getMinutes(); // 분
+      let s = d.getSeconds(); // 초
+  
+      // 0 ~ 9까지는 앞에 0을 표시??
+      if(s < 10) {
+        s = '0' + s; 
+      }
+  
+      /* 과제
+        1) 오전/오후 표시?? - 24시간제 if()
+        2) 알람기능: 50분이 되면 '휴식시간' 경고창 띄우기
+      */
         if (h < 13) {
             $('.time').html('AM');
         }
         if (h > 13) {
             $('.time').html('PM');
         }
-
-        //알람기능 - 50분이되면 경고창 : <휴식시간>
-     
         
-        //시간표시
-        $('.hour').html(h);
-        $('.min').html(m);
-        $('.sec').html(s); 
-    })
-    })
 
-
+      // 응용작품 만들기
+  
+      // 시간표시
+      $('.hour').html(h);
+      $('.min').html(m);
+      $('.sec').html(s);
+    }, 1000);
+  
+  })
+  
+  
